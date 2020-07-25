2ee89a51186c9cb514e30172a70d3255ece2d8de
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-07-25 17:17:54 +0800

    Added 4 links on the age.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,13 +3,17 @@ import { Routes, RouterModule } from '@angular/router';
 import { TemplateDrivenComponent } from './template-driven/template-driven.component';
 import { ModelDrivenComponent } from './model-driven/model-driven.component';
 import { TypedFormsComponent } from './typed-forms/typed-forms.component';
+import { BasicComponent } from './basic/basic.component';
+import { HomeComponent } from './home/home.component';
 
 const routes: Routes = [
+  {path: 'home', component: HomeComponent},
+  {path: 'basic', component: BasicComponent},
   {path: 'template-driven', component: TemplateDrivenComponent},
   {path: 'model-driven', component: ModelDrivenComponent},
   {path: 'typed-form', component: TypedFormsComponent},
-  {path: '', redirectTo: 'template-driven', pathMatch: 'full'},
-  {path: '**', redirectTo: 'template-driven'},
+  {path: '', redirectTo: 'home', pathMatch: 'full'},
+  {path: '**', redirectTo: 'home'},
 ];
 
 @NgModule({
