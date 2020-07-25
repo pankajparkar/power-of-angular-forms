95a0c6fcb90a918294167c2e053eaa4fa060567e
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-07-25 19:28:31 +0800

    Added model driven forms.

--- src/app/model-driven/model-driven.component.ts
+++ src/app/model-driven/model-driven.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
 
 @Component({
   selector: 'poaf-model-driven',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ModelDrivenComponent implements OnInit {
 
-  constructor() { }
+  // FormControl
+  // FormGroup
+  // FormArray
+  profileForm: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
+
+  toggle({checked}) {
+    const emailControl = this.profileForm.get('email');
+    console.log(emailControl);
+    if (checked) {
+      // Add validator
+      emailControl.setValidators([Validators.required]);
+    } else {
+      // remove validator
+      emailControl.clearValidators();
+    }
+    emailControl.updateValueAndValidity();
+  }
+
+  addLanguage() {
+    const languagesFormArray = this.profileForm.get('languages') as FormArray;
+    const newLanguage = this.fb.group({
+      language: '',
+      fluency: ''
+    });
+    languagesFormArray.push(newLanguage);
+  }
+
+  getLanguages() {
+    return (this.profileForm.get('languages') as FormArray).controls
+  }
 
   ngOnInit(): void {
+    this.profileForm = this.fb.group({
+      firstName: new FormControl('', [Validators.required]),
+      lastName: ['', [Validators.required]],
+      email: [''],
+      address: ['', [Validators.required]],
+      address2: ['', [Validators.required]],
+      state: ['', [Validators.required]],
+      city: ['', [Validators.required]],
+      postalCode: ['', [Validators.required]],
+      languages: this.fb.array([])
+    });
   }
 
 }
