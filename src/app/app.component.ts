16757797aa1e6df7f93b787f98af17b33b0329f4
Pankaj Parkar <parkarpankaj3@gmail.com>
2020-07-25 15:31:47 +0800

    Added initial CLI commit.

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'poaf-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'power-of-angular-forms';
+}
